b33f0359df8a6f22226805b49fcd9b0b9cb550e1
fabaoZion <fabaoanalista@gmail.com>
2023-08-28 13:02:06 -0300

    init project

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+    <div >
+      <h1><strong>Portifolio Fabio Humberto de Carvalho</strong></h1>
+      <p class="font-sans">
+        Formação em Sistemas de Infomações UFOP
+      </p>
+      <P>
+        Gestão de Projetos - MBA - USP
+      </P>
+        
+    </div>
+  </template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <li>
+    <NuxtLink to="/about">Sobre</NuxtLink>
+   </li>
+</template>
+<style>
+
+li {
+  margin: 0 0.5rem;
+  padding: 0.25rem;
+  font-size: 1.2rem;
+}
+</style>
